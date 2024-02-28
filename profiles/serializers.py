1c8f5705ab5a2d807576f2ea8c07c8d1cf46473b
Luke Skinner <laskinner@gmail.com>
2024-02-28 23:25:41 +0100

    Set the owner field to read-only in the serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,6 +4,9 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    # Set the owner field to read-only
+    owner = serializers.ReadOnlyField(source="owner.username")
+
     class Meta:
         model = Profile
         fields = ["id", "owner", "name", "content", "image", "created_at", "updated_at"]
