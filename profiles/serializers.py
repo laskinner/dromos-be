b274c6deb676b78d7543b326cd53b1e46320e7b1
Luke Skinner <laskinner@gmail.com>
2024-02-29 00:26:57 +0100

    Finished building profile resource

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,7 +6,20 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     # Set the owner field to read-only
     owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ["id", "owner", "name", "content", "image", "created_at", "updated_at"]
+        fields = [
+            "id",
+            "owner",
+            "name",
+            "content",
+            "image",
+            "created_at",
+            "updated_at",
+            "is_owner",
+        ]
+
+    def get_is_owner(self, obj):
+        return self.context["request"].user == obj.owner
--- profiles/views.py
+++ profiles/views.py
@@ -2,8 +2,15 @@
 from rest_framework import viewsets
 from .models import Profile
 from .serializers import ProfileSerializer
+from dromos_be.permissions import IsOwnerOrReadOnly
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]  # Apply the custom permission
+
+    def get_serializer_context(self):
+        context = super(ProfileViewSet, self).get_serializer_context()
+        context.update({"request": self.request})
+        return context
