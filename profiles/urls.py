97f30377301aafe6896095169f86aaefb6bb4acc
Luke Skinner <laskinner@gmail.com>
2024-03-17 01:06:03 +0100

    Finished creating now views and api endpoints for simplejwt

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet
+from .views import ProfileViewSet, user_details
 
 router = DefaultRouter()
 router.register(r"", ProfileViewSet, basename="profile")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("user/", user_details, name="user-details"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,9 @@ from rest_framework import viewsets
 from .models import Profile
 from .serializers import ProfileSerializer
 from dromos_be.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -14,3 +17,18 @@ class ProfileViewSet(viewsets.ModelViewSet):
         context = super(ProfileViewSet, self).get_serializer_context()
         context.update({"request": self.request})
         return context
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def user_details(request):
+    # Access the user from the request
+    user = request.user
+    # Return the user details you need
+    return Response(
+        {
+            "username": user.username,
+            "email": user.email,
+            # Include other details from the user model or related models as needed
+        }
+    )
