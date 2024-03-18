ef3302563ccdbc0bb7bf3f9f2fe564c685553e08
Luke Skinner <laskinner@gmail.com>
2024-03-18 02:07:56 +0100

    Added a test endpoint my profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from django.http import JsonResponse
 from .views import ProfileViewSet, user_details
 from rest_framework.routers import DefaultRouter
+
+
+# Define the test_view function
+def test_view(request):
+    return JsonResponse({"message": "Test endpoint is working"})
+
 
 router = DefaultRouter()
 router.register(r"", ProfileViewSet, basename="profile")
@@ -8,4 +15,6 @@ router.register(r"", ProfileViewSet, basename="profile")
 urlpatterns = [
     path("", include(router.urls)),
     path("user/", user_details, name="user-details"),
+    # Add the test endpoint
+    path("test/", test_view, name="test-view"),
 ]
