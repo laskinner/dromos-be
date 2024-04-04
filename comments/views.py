9935059a818da746050e158710bdbd361dabf437
Luke Skinner <laskinner@gmail.com>
2024-04-05 00:28:52 +0200

    Added new filter to return comments by node id  in comment view

--- comments/views.py
+++ comments/views.py
@@ -9,5 +9,19 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
+    def get_queryset(self):
+        """
+        Optionally restricts the returned comments to a given node,
+        by filtering against a `node` query parameter in the URL.
+        """
+
+        # Ensure base implementation is called
+        queryset = super().get_queryset()
+        node_id = self.request.query_params.get("node")
+        if node_id is not None:
+            queryset = queryset.filter(node__id=node_id)
+        return queryset
+
     def perform_create(self, serializer):
+        # Assuming `owner` is automatically added from the logged-in user
         serializer.save(owner=self.request.user)
