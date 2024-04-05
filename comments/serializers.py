ad97be0ec1a51bd3d7a1208cc16998621dad1025
Luke Skinner <laskinner@gmail.com>
2024-04-05 15:55:49 +0200

    Adjusted back-end to handle owner authentification for comments, and make own read-only

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,25 +1,28 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Comment
-from django.conf import settings
+
+User = get_user_model()
 
 
-# Base Comment Serializer
 class CommentSerializer(serializers.ModelSerializer):
     profile_id = serializers.SerializerMethodField()
     profile_image = serializers.SerializerMethodField()
+    owner_username = serializers.ReadOnlyField(source="owner.username")
 
     class Meta:
         model = Comment
         fields = (
             "id",
             "node",
-            "owner",
+            "owner_username",
             "content",
             "created_at",
             "updated_at",
             "profile_id",
             "profile_image",
         )
+        read_only_fields = ("owner",)
 
     def get_profile_id(self, obj):
         return obj.owner.profile.id
@@ -32,11 +35,3 @@ class CommentSerializer(serializers.ModelSerializer):
             if request
             else profile_image_url
         )
-
-
-# Detailed Comment Serializer that locks a comment to a specific node
-class CommentDetailSerializer(CommentSerializer):
-    node = serializers.ReadOnlyField(source="node.id")
-
-    class Meta(CommentSerializer.Meta):
-        fields = CommentSerializer.Meta.fields + ("node",)
